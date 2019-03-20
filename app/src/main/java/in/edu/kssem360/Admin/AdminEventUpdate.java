3e05dcfb0506a1f954dce147cf913498ce57f633
DropCodes <sagardb50@gmaill.com>
2019-03-21 01:54:45 +0530

    Firebase Storage for uploading posters

--- app/src/main/java/in/edu/kssem360/Admin/AdminEventUpdate.java
+++ app/src/main/java/in/edu/kssem360/Admin/AdminEventUpdate.java
@@ -1,8 +1,13 @@
 package in.edu.kssem360.Admin;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.view.View;
@@ -14,19 +19,28 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 import in.edu.kssem360.R;
 
 public class AdminEventUpdate extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
@@ -35,16 +49,22 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
     private TextView mDateText;
     int day, month, year, hour, minute;
     int dayFinal, monthFinal, yearFinal;
-    private String format = "" ;
+    private String format = "";
     private String date_time;
     private TextInputEditText mEventName, mEventType, mReqFee, mVenue, mNumParticipate, mCordinatName, mCordinatNumber, mDescription;
+    private CircleImageView mPoaterImage;
 
     private FirebaseDatabase mDatabase;
     private DatabaseReference mRef;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
-
+    public FirebaseStorage mStorage;
+    public StorageReference mStorageRef;
     private String UID;
+    public Uri uri;
+    public String image_url;
+
+    private static int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,9 +82,11 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
         mCordinatNumber = findViewById(R.id.event_co_ord_number);
         mDescription = findViewById(R.id.event_description);
         mPostEvent = findViewById(R.id.event_post);
+        mPoaterImage = findViewById(R.id.poster);
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance();
+        mStorage = FirebaseStorage.getInstance();
         mRef = mDatabase.getReference().child("event").push();
         mUser = mAuth.getCurrentUser();
         UID = mUser.getUid();
@@ -88,6 +110,15 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
             @Override
             public void onClick(View v) {
                 postEvent();
+            }
+        });
+        mPoaterImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "SELECT IMAGE"), REQUEST_CODE);
             }
         });
     }
@@ -114,8 +145,8 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
             map.put("co_ord_name", event_co_name);
             map.put("co_ord_number", event_co_number);
             map.put("desc", event_description);
-            map.put("date",date_time);
-            map.put("admin",UID);
+            map.put("date", date_time);
+            map.put("admin", UID);
 
             mRef.updateChildren(map).addOnCompleteListener(new OnCompleteListener() {
                 @Override
@@ -133,6 +164,60 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
                     Toast.makeText(AdminEventUpdate.this, e.toString(), Toast.LENGTH_SHORT).show();
                 }
             });
+        } else {
+            Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+            final ProgressDialog imageProgress = new ProgressDialog(AdminEventUpdate.this);
+            imageProgress.setMessage("Please wait...");
+            imageProgress.setTitle("Uploading Image");
+            imageProgress.setIcon(R.mipmap.ic_launcher);
+            imageProgress.setCanceledOnTouchOutside(false);
+            imageProgress.show();
+            uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                final byte[] image_byte = baos.toByteArray();
+
+                mStorageRef = mStorage.getReference().child("fest").child("poster").child(random()+".jpeg");
+
+                UploadTask uploadTask = mStorageRef.putBytes(image_byte);
+                uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if (task.isSuccessful()) {
+
+                            mStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri download_uri) {
+                                    image_url = download_uri.toString();
+                                    mRef.child("image").setValue(image_url);
+                                    mPoaterImage.setImageURI(uri);
+                                    imageProgress.dismiss();
+                                    Toast.makeText(AdminEventUpdate.this, "Image uploaded successfully", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(AdminEventUpdate.this, "error while uploading image please try again", Toast.LENGTH_LONG).show();
+                            imageProgress.hide();
+                        }
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(AdminEventUpdate.this, "error while getting image please try again", Toast.LENGTH_LONG).show();
+            }
+
         }
     }
 
@@ -171,5 +256,17 @@ public class AdminEventUpdate extends AppCompatActivity implements DatePickerDia
         date_time = dayFinal + "/" + monthFinal + "/" + yearFinal + " at " + time;
         mDateText.setText(date_time);
 
+    }
+
+    public static String random(){
+        Random generator = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(10);
+        char tempChar;
+        for(int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96)+32);
+            stringBuilder.append(tempChar);
+        }
+        return stringBuilder.toString();
     }
 }
